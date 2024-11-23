fba6ef174506bc6201c2cfbb3b95f82adb6c11f2
Manish Gupta <Guptadmanish2710@gmail.com>
2024-11-23 21:41:13 +0530

    Update PhotosGallery.jsx

--- src/pages/Photosgallery/PhotosGallery.jsx
+++ src/pages/Photosgallery/PhotosGallery.jsx
@@ -25,7 +25,7 @@ const PhotosGallery = () => {
   const [searchQuery, setSearchQuery] =
     useState("");
   const [sortOption, setSortOption] =
-    useState("likes"); // Default sort by likes
+    useState("default"); // Default sort by likes
   const [
     isNextPageError,
     setIsNextPageError,
@@ -52,7 +52,7 @@ const PhotosGallery = () => {
 
   if (isLoading)
     return (
-      <div className='spinner-first'>
+      <div className='flex justify-center items-center'>
         <Spinner />
       </div>
     );
@@ -90,66 +90,75 @@ const PhotosGallery = () => {
           new Date(a.created_at)
         ); // Sort by date
       }
-      return 0; // Default case
+      else if(sortOption==='default')
+      return filteredImages; // Default case
     });
 
 
   return (
-  <div className="main">
-  <div className='main-div'>
-    {" "}
-    <h1 className='heading'>
-      {" "}
-      Picture Gallery{" "}
-    </h1>{" "}
-    <div className='sort-search-div'>
-      {" "}
-      <input
-        type='text'
-        placeholder='Search images...'
-        value={searchQuery}
-        onChange={(e) =>
-          setSearchQuery(e.target.value)
-        }
-        className='search'
-      />{" "}
-      <select
-        value={sortOption}
-        onChange={(e) =>
-          setSortOption(e.target.value)
-        }
-        className='sort'>
-        <option value='likes'>
-          Sort by Likes
-        </option>
-        <option value='date'>
-          Sort by Date
-        </option>
-      </select>
-    </div>
-    <div className='imageGrid'>
-      {sortedImages.map((image) => (
-        <PhotoCard
-          key={image.id}
-          image={image}
-        />
-      ))}
-    </div>
-    {isFetchingNextPage &&
-      !isNextPageError && (
-        <div className='spinner'>
-          <Spinner />
+    <div className='main'>
+      <div className='main-div'>
+        {" "}
+        <h1 className='heading'>
+          {" "}
+          Picture Gallery{" "}
+        </h1>{" "}
+        <div className='sort-search-div'>
+          {" "}
+          <input
+            type='text'
+            placeholder='Search images...'
+            value={searchQuery}
+            onChange={(e) =>
+              setSearchQuery(
+                e.target.value
+              )
+            }
+            className='search'
+          />{" "}
+          <select
+            value={sortOption}
+            placeholder='Sort'
+            onChange={(e) =>
+              setSortOption(
+                e.target.value
+              )
+            }
+            className='sort'>
+            <option value='default'>
+              Sort by Default
+            </option>
+            <option value='likes'>
+              Sort by Likes
+            </option>
+            <option value='date'>
+              Sort by Date
+            </option>
+          </select>
         </div>
-      )}
-    {isNextPageError && (
-      <div className='error-network'>
-        Network issue. Unable to load
-        more images.
+        <div className='imageGrid'>
+          {sortedImages.map((image) => (
+            <PhotoCard
+              key={image.id}
+              image={image}
+            />
+          ))}
+        </div>
+        {isFetchingNextPage &&
+          !isNextPageError && (
+            <div className='spinner'>
+              <Spinner />
+            </div>
+          )}
+        {isNextPageError && (
+          <div className='error-network'>
+            Network issue. Unable to
+            load more images.
+          </div>
+        )}
       </div>
-    )}
-      </div>
-      </div>
-);
+    </div>
+  );
 };
 
 export default PhotosGallery;
